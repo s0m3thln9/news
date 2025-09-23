fc0da1828538f18726eb721399b73bfb7421eb71
s0m3thlin9 <dunlock67@gmail.com>
2025-09-23 21:24:00 +0300

    add font and seo, change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from "@eslint/eslintrc"
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -7,51 +7,51 @@ const compat = new FlatCompat({
 const eslintConfig = [
   {
     ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
     ],
   },
 
   ...compat.config({
     extends: [
-      'next',
-      'next/core-web-vitals',
-      'next/typescript',
-      'plugin:prettier/recommended',
-      'plugin:jsx-a11y/recommended',
+      "next",
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:prettier/recommended",
+      "plugin:jsx-a11y/recommended",
     ],
-    plugins: ['prettier', 'jsx-a11y'],
+    plugins: ["prettier", "jsx-a11y"],
     rules: {
-      'prettier/prettier': [
-        'error',
+      "prettier/prettier": [
+        "error",
         {
-          trailingComma: 'all',
+          trailingComma: "all",
           semi: false,
           tabWidth: 2,
-          singleQuote: true,
+          singleQuote: false,
           printWidth: 80,
-          endOfLine: 'auto',
-          arrowParens: 'always',
-          plugins: ['prettier-plugin-tailwindcss'],
+          endOfLine: "auto",
+          arrowParens: "always",
+          plugins: ["prettier-plugin-tailwindcss"],
         },
         {
           usePrettierrc: false,
         },
       ],
-      'jsx-a11y/anchor-is-valid': 'off',
-      '@next/next/no-img-element': 'off',
-      'jsx-a11y/no-static-element-interactions': 'off',
-      'jsx-a11y/click-events-have-key-events': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'jsx-a11y/alt-text': 'warn',
-      'jsx-a11y/aria-props': 'warn',
-      'jsx-a11y/aria-proptypes': 'warn',
-      'jsx-a11y/aria-unsupported-elements': 'warn',
-      'jsx-a11y/role-has-required-aria-props': 'warn',
-      'jsx-a11y/role-supports-aria-props': 'warn',
+      "jsx-a11y/anchor-is-valid": "off",
+      "@next/next/no-img-element": "off",
+      "jsx-a11y/no-static-element-interactions": "off",
+      "jsx-a11y/click-events-have-key-events": "off",
+      "react/react-in-jsx-scope": "off",
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/aria-props": "warn",
+      "jsx-a11y/aria-proptypes": "warn",
+      "jsx-a11y/aria-unsupported-elements": "warn",
+      "jsx-a11y/role-has-required-aria-props": "warn",
+      "jsx-a11y/role-supports-aria-props": "warn",
     },
   }),
 ]
